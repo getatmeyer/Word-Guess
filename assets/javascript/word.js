f2d1dafe3b8a509bae020452395864aa092bba58
nathan meyer <nmeyer@nathans-MacBook-Air.local>
2019-04-22 19:55:52 -0700

    updates

--- assets/javascript/word.js
+++ assets/javascript/word.js
@@ -7,35 +7,108 @@ var selectWords = [
     "extravaganza",
     "tranquilizer",
     "shadowboxing",
+    "chickenboxes",
+    "complexifies",
+    "steeplejacks",
 ];
-const maxError= 12; // The number of tries user get to guess
 
-   // 
+var losses= 0; // The number of tries user get to guess
 var guessedLetter = []; // Stores the letters the user guess
-var currentWordIndex;
-var guessedWord = [];  // the word that matches Word
+var currentWordIndex;  // Index of the current word in the array
+var guessedWord = [];  // the word that matches selectWord
 var remainingGuesses = 0; //How many tries the user has left
-var gameStarted = false;
-var hasFinished = false;
-var wins = 0
+var gameStarted = false;  // FLag to indict if the game has started
+var hasFinished = false;  // Flag for press any key to try again
+var wins = 0              // How many wins the user racks up
+
+var userText = document.getElementById("blank"); 
+var lossesText = document.getElementById("losses");
+
+var directionsText = document.getElementById("directions-text");
 
 // -- GAME START FUNCTION -- \\ 
 function resetGame () {
-    remainingGuesses = maxTries;
+
+    remainingGuesses = losses;
     gameStarted = false;
     
     //Math.floor formula
     currentWordIndex = Math.floor(Math.random() * (selectWords.length));
-    console.log(currentWordIndex.selectWords = "TEST");
 
+    remainingGuesses = 12;
+
+    // clear out array
     guessedLetter = [];
     guessedWord = [];
 
     for (var i = 0; i < selectWords[currentWordIndex].length; i++) {
-        guessedWord.push("_");
-        console.log(selectWords.currentWordIndex = "WERID");
-        
+        console.log(selectWords[currentWordIndex]);
+        guessedWord.push();
     }
+    }resetGame();
 
-}
+    // -- RENDER SCORE -- \
+    var renderScore = function () {
 
+        currentWordIndex = Math.floor(Math.random() * (selectWords.length));
+        console.log(selectWords.length);
+
+        // if the user guess right letter
+        if (guessedLetter === currentWordIndex) {
+            console.log(guessedLetter + "win condition!");
+
+            alert("You're lucky!");
+            wins++;
+        
+        }
+        // if the user doesn't get right letter
+        else if (guessedLetter != currentWordIndex) {
+            console.log(guessedLetter + "you get wrong letter");
+
+            remainingGuesses -= 1;
+
+            if (remainingGuesses === 0) {
+                alert("Try again");
+
+                losses++;
+
+            }currentWordIndex.push(guessedLetter);
+        }resetGame();
+    }
+    // --  OnKeyUP FUNCTION -- \
+    document.onkeyup = function(event) {
+        
+        userGuess = event.key
+        console.log(userGuess);
+        userText.textContent = event.key.toLowerCase();
+
+        // Randomly chooses a choice from the options array. This is the Computer's guess.
+        // var guessedWord = selectWords[Math.floor(Math.random() * selectWords.length)];
+        // currentWordIndex = Math.floor(Math.random() * (selectWords.length));
+        // console.log(selectWords.length);
+        
+         // Hide the directions
+         directionsText.textContent = "";
+         renderScore();
+         
+        //  guessedWord.push(guessedLetter)
+      }
+      function displayGuessesLetter () { // function to display letterGuessed}
+            document.getElementById("letterGuess").innerHTML = "Guessed Letter : " + guessedLetter;
+            }displayGuessesLetter();
+
+            function displayWins () { // function to display wins}
+            document.getElementById("totalWins").innerHTML = "Wins : " + wins;
+            }displayWins();
+
+            function displaylosses () { //function to display loss in HTML
+                document.getElementById("losses").innerHTML = "Losses : " + losses;
+                }displaylosses();
+
+
+            function displayRemaininguess () { //function to display loss in HTML
+                document.getElementById("remainingGuesses").innerHTML = " Remaining Guesses : " + remainingGuesses;
+                }displayRemaininguess();
+
+
+    
